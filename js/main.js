81ce420706e7cc127067b33d43a85d9538a593eb
Dustin Ruetz <dustinruetz@gmail.com>
2016-05-24 12:50:33 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function() {
+	$(".nav-links a").smoothScroll();
+	// Click the Menu button ...
+	$(".menu-open").on("click", function() {
+		// ... open the navbar
+		$(".nav-links").toggleClass("show-menu");
+	});
+	// Click the Close button ...
+	$(".menu-close").on("click", function() {
+		// ... close the navbar
+		$(".nav-links").toggleClass("show-menu");
+	});
+	// Click on a link in the navbar ...
+	$(".nav-links a").on("click", function() {
+		// ... close the navbar
+		$(".nav-links").removeClass("show-menu");
+	});
+	// Click on the easter egg ...
+	$(".easter-egg").on("click", function() {
+		// ... toggle the .translucent-overlay
+		$(".translucent-overlay").toggleClass("show-bars");
+		$("#page-header").toggleClass("front-bars");
+	});
+});
